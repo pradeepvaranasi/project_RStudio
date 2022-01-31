b6b82c2f575124f58f7e84918eb0854ecf09b86b
pradeepvaranasi <pradeepvaranasi91@gmail.com>
2022-01-31 15:24:18 +0530

    added datatypes

--- refresher1.R
+++ refresher1.R
@@ -1,1 +1,18 @@
-print("hello world")
+x = 9
+View(x)
+
+# Different datatypes and class() function to check respective datatype
+
+objA = 'abc'
+objB = '123'
+objC = 'TRUE'
+
+obj1 = 1234
+obj2 = 21312132
+obj3 = 3123123321
+obj4 = 321938123.3423
+obj5 = 1323432443.34234
+obj6 = 3123121L
+
+objT = TRUE
+
